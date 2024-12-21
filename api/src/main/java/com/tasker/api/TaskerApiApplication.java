b5ba317b8c1979ddde57acd30a96404bc0a44884
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-12-21 12:56:42 -0300

    feat(api): add project files

---
+++ api/src/main/java/com/tasker/api/TaskerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tasker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskerApiApplication.class, args);
+	}
+
+}
