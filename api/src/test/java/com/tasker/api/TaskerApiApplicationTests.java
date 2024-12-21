ec07f39c9903f3a0f45c13b5b2a5dda21ccb2603
Ricardo Brasileiro <jricardobgoncalves@gmail.com>
2024-12-21 13:08:00 -0300

    test(api): configure test database to main class

--- api/src/test/java/com/tasker/api/TaskerApiApplicationTests.java
+++ api/src/test/java/com/tasker/api/TaskerApiApplicationTests.java
@@ -1,9 +1,11 @@
 package com.tasker.api;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureTestDatabase
 class TaskerApiApplicationTests {
 
 	@Test
